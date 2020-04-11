0cfa05124581b3a44254cf108c6e5fa6d60842e2
Kalia1110 <56771744+Kalia1110@users.noreply.github.com>
2020-04-11 23:06:01 +0530

    initial commit

---
+++ MVCProject05_UrlFilenameViewController/src/main/java/com/kcp/service/WishMeaagaeService.java
@@ -0,0 +1,21 @@
+package com.kcp.service;
+
+import java.util.Calendar;
+
+public class WishMeaagaeService {
+	public String getWishMeaasge(String userName) {
+		String WishMsg = null;
+		Calendar calendar = Calendar.getInstance();
+		int hr = calendar.get(Calendar.HOUR_OF_DAY);
+		if (hr <= 12) {
+			WishMsg = "good morning "+userName;
+		} else if (hr <= 16) {
+			WishMsg = "good afternoon "+userName;
+		} else if (hr <= 20) {
+			WishMsg = "good evning "+userName;
+		} else {
+			WishMsg = "good night "+userName;
+		}
+		return WishMsg;
+	}
+}
